ae84c8b34906f0a6eed985272b4a5d6087a3d9cb
habitatchen <habitatchen@163.com>
2018-09-18 17:21:17 +0800

    add icons

---
+++ src/pages/home/components/icons.vue
@@ -0,0 +1,131 @@
+<template>
+    <div class="icons">
+    <swiper :options="swiperOption" class="swiper-containner">
+      <swiper-slide v-for="(page,index) of pages" :key="index">
+        <div class="icon" 
+            v-for="item of page" 
+            :key="item.id"
+            >
+            <div class="icon-img">
+                <img :src="item.imgUrl" alt="" class="icon-img-content">
+            </div>
+            <p class='icon-desc'>{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: "HomeIcons",
+  computed: {
+      pages () {
+          const pages = []
+          this.iconList.forEach((item,index)=>{
+              const page = Math.floor(index / 8)
+              if (!pages[page]) {
+                  pages[page] = []
+              }
+              pages[page].push(item)
+          })
+          return pages
+      }
+  },
+  data() {
+    return {
+      swiperOption: {
+        pagination: ".swiper-pagination",
+      },
+      iconList:[{
+          id:'0001',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png',
+          desc:'无限疯狂'
+      },{
+          id:'0002',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png',
+          desc:'无限风光'
+      },{
+          id:'0003',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png',
+          desc:'无限风光'
+      },{
+          id:'0004',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png',
+          desc:'无限风光'
+      },{
+          id:'0005',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png',
+          desc:'无限风光'
+      },{
+          id:'0006',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png',
+          desc:'无限风光'
+      },{
+          id:'0007',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png',
+          desc:'无限风光'
+      },{
+          id:'0008',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png',
+          desc:'无限风光'
+      },{
+          id:'0008',
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png',
+          desc:'无限风光'
+      },
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped lang='stylus'>
+.icons >>> .swiper-containner {
+    height: 0;
+    padding-bottom: 50%;
+}
+
+.icon {
+    height: 0;
+    position: relative;
+    overflow: hidden;
+    float: left;
+    width: 25%;
+    padding-bottom: 25%;
+
+    .icon-desc {
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        height: 0.44rem;
+        line-height: 0.44rem;
+        color: #333;
+        text-align: center;
+        overflow hidden 
+        white-space nowrap 
+        text-overflow ellipsis
+    }
+
+    .icon-img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0.44rem;
+        box-sizing: borderbox;
+        padding: 0.1rem;
+
+        .icon-img-content {
+            display: block;
+            height: 100%;
+            margin: 0 auto;
+        }
+    }
+}
+</style>
+
+
